a9aabdc83d9a7d758be0a49214a2d84da91ab74e
Nikki Everett <nikki@televet.com>
2022-04-22 11:11:54 -0500

    Adds gtag for tracking and analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,22 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sass`,
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-DD5PBNSZP0", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "3469248336",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/articles`,
